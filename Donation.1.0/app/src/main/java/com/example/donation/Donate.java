f7f8816cb57e4758f515e29d771a291a3a94359e
titan282 <nguyencongtan1002@gmail.com>
2022-03-11 21:34:30 +0700

    Donation 1.0

---
+++ Donation.1.0/app/src/main/java/com/example/donation/Donate.java
@@ -0,0 +1,110 @@
+package com.example.donation;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.donation.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+
+public class Donate extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private Button donateButton;
+    private RadioGroup paymentMethod;
+    private NumberPicker amountPicker;
+    private ProgressBar progressBar;
+    private int totalDonated=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Donate 1.0
+        donateButton = (Button) findViewById(R.id.donateButton);
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setMax(10000);
+        if (donateButton != null)
+        {
+            Log.v("Donate", "Really got the donate button");
+        }
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+        return false;
+    }
+
+    //Donation 1.0
+    public void donateButtonPressed(View view){
+        int amount = amountPicker.getValue();
+        int radioId = paymentMethod.getCheckedRadioButtonId();
+        totalDonated +=amount;
+        progressBar.setProgress(totalDonated);
+        String method = radioId == R.id.Paypal ? "Paypal" : "Direct";
+        Log.v("Donate", "Donate Pressed with amount: "+ amount + " method: "+method);
+        Log.v("Donate", "Total donated = "+ totalDonated);
+    }
+}
